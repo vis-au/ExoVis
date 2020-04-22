de7eb6ed01d96f117d225061dea0dbe5737454c7
Mahog <6837928+Mahog@users.noreply.github.com>
2020-04-22 13:46:25 +0200

    d3 rewrite of frontend

--- frontend/main_lightcurve.js
+++ frontend/main_lightcurve.js
@@ -2,12 +2,135 @@ const MAX_PAA = 12;
 const MAX_SAX = 14;
 
 const MATRIX_DOMAIN = [0.25, 89.11];
-const MATRIX_CELL_SIZE = 24;
+const MATRIX_CELL_SIZE = 34;
 const MATRIX_HISTOGRAM_HEIGHT = 50;
 const MATRIX_HISTOGRAM_WIDTH = MATRIX_CELL_SIZE - 10;
-const MATRIX_WIDTH = (MAX_PAA) * (MATRIX_CELL_SIZE);
+const MATRIX_WIDTH = (MAX_PAA) * (MATRIX_CELL_SIZE) * 2.5;
 const MATRIX_HEIGHT = (MAX_SAX) * (MATRIX_CELL_SIZE);
 const MATRIX_HISTOGRAM_COLOR = "grey";
+
+const svg = d3.select("div#canvas").append("svg")
+  .attr("width", MATRIX_WIDTH)
+  .attr("height", MATRIX_HEIGHT)
+  .style("background", "white");
+
+const barSize = 100;
+const paddingX = 30;
+const paddingY = 50;
+
+const scaleX = d3.scaleLinear()
+  .domain([0, MAX_PAA])
+  .range([paddingX, MATRIX_WIDTH - barSize - paddingX]);
+
+const scaleY = d3.scaleLinear()
+  .domain([3, MAX_SAX + 1])
+  .range([paddingY + barSize, MATRIX_HEIGHT - paddingY]);''
+
+const xStep = scaleX(2) - scaleX(1);
+const yStep = scaleY(2) - scaleY(1);
+
+const color = d3.scaleSequentialLog(d3.interpolateYlGnBu)
+  .domain(MATRIX_DOMAIN);
+
+
+// let dummyData = getDummyData();
+// sendRealData(dummyData);
+// console.log(getTransformedData(dummyData))
+
+
+function render(listData) {
+  const data = getTransformedData(listData);
+  const meanXProgress = d3.range(MAX_PAA).map(d => Math.random());
+  const meanYProgress = d3.range(2, MAX_SAX).map(d => Math.random());
+
+  renderAxes();
+  updateMatrix(data);
+  renderBars(meanXProgress, meanYProgress);
+}
+
+function updateMatrix(data) {
+
+  svg.selectAll("g.matrix").remove();
+
+  const matrix = svg.append("g").attr("class", "matrix");
+
+  matrix.selectAll("rect.cell").data(data).join("rect")
+    .attr("class", "cell")
+    .attr("x", d => scaleX(d.omega))
+    .attr("y", d => scaleY(d.alpha))
+    .attr("width", xStep)
+    .attr("height", yStep)
+    .attr("fill", d => color(d.error))
+    .attr("stroke", "none");
+
+  matrix.selectAll("text.label").data(data).join("text")
+    .attr("class", "label")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 11)
+    .attr("text-anchor", "middle")
+    .attr("x", d => scaleX(d.omega) + xStep/2)
+    .attr("y", d => scaleY(d.alpha) + yStep * 0.66)
+    .attr("fill", d => d.error < 30 ? "black" : "white")
+    .text(d => (d.error + "").slice(0, 4));
+}
+
+
+function renderAxes() {
+  const xAxisGenerator = d3.axisBottom(scaleX);
+  const yAxisGenerator = d3.axisLeft(scaleY);
+
+  svg.selectAll("g.axis").remove();
+
+  const axisX = svg.append("g")
+    .attr("class", "axis x")
+    .attr("transform", `translate(0, ${MATRIX_HEIGHT - paddingY})`)
+    .call(xAxisGenerator);
+
+  const axisY = svg.append("g")
+    .attr("class", "axis y")
+    .attr("transform", `translate(${paddingX}, 0)`)
+    .call(yAxisGenerator);
+
+  axisX.append("text")
+    .text("omega")
+    .attr("x", paddingX + MATRIX_WIDTH/2)
+    .attr("y", MATRIX_HEIGHT + paddingY);
+}
+
+const barScale = d3.scaleLinear().domain([0, 1]).range([0, barSize]);
+
+function renderBars(meanXProgress, meanYProgress) {
+  const barColor = "#afafaf";
+  const barStroke = "#ccc";
+
+  svg.selectAll("g.bars").remove();
+
+  const barsX = svg.append("g").attr("class", "bars x");
+  const barsY = svg.append("g").attr("class", "bars y");
+
+  barsX.selectAll("rect.bar.x").data(meanXProgress).join("rect")
+    .attr("class", "bar x")
+    .attr("x", (d, i) => scaleX(i))
+    .attr("y", d => barSize + paddingY - barScale(d))
+    .attr("width", xStep)
+    .attr("height", d => barScale(d))
+    .attr("fill", barColor)
+    .attr("stroke", barStroke);
+
+  barsY.selectAll("rect.bar.y").data(meanYProgress).join("rect")
+    .attr("class", "bar y")
+    .attr("x", scaleX(scaleX.domain()[1]))
+    .attr("y", (d, i) => scaleY(i + 3))
+    .attr("width", d => barScale(d))
+    .attr("height", yStep)
+    .attr("fill", barColor)
+    .attr("stroke", barStroke);
+
+  barsX.append("text")
+    .attr("transform")
+}
+
+render(getDummyData());
 
 
 /**
@@ -18,7 +141,7 @@ function getDummyData() {
 
   for (let paa = 0; paa < MAX_PAA; paa++) {
     for (let sax = 0; sax < MAX_SAX; sax++) {
-      dummyData.push(Math.random());
+      dummyData.push(Math.random() * MATRIX_DOMAIN[1] - MATRIX_DOMAIN[0]);
     }
   }
 
@@ -35,177 +158,20 @@ function getTransformedData(arrayData) {
 
   for (let paa = 0; paa < MAX_PAA; paa++) {
     for (let sax = 0; sax < MAX_SAX; sax++) {
-      transformedData.push({
-        "alpha": sax,
-        "omega": paa,
-        "error": arrayData[sax * MAX_SAX + paa],
-        "progress": 100
-      });
+      if (arrayData[sax * MAX_SAX + paa] !== undefined) {
+        transformedData.push({
+          "alpha": sax + 3,
+          "omega": paa,
+          "error": arrayData[sax * MAX_SAX + paa],
+          "progress": 100
+        });
+      }
     }
   }
 
   return transformedData;
 }
 
-// reference to the vega embed view, is set by the vegaEmbed promise on rendering.
-let view = null;
-
-const paaScale = {
-  "domain": [0, MAX_PAA]
-};
-const saxScale = {
-  "domain": [MAX_SAX, 0]
-};
-
-function sendRealData(realData) {
-  const spec = {
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-    "data": {
-      values: getTransformedData(realData)
-    },
-    "vconcat": [
-      {
-        "mark": "bar",
-        "width": MATRIX_WIDTH,
-        "height": MATRIX_HISTOGRAM_HEIGHT,
-        "encoding": {
-          "x": {
-            "bin": {
-              "maxbins": MAX_PAA
-            },
-            "field": "omega",
-            "type": "quantitative",
-            "axis": null,
-            scale: paaScale
-          },
-          "y": {
-            "aggregate": "mean",
-            "field": "progress",
-            "type": "quantitative",
-            "scale": {
-              "domain": [0, 100]
-            },
-            "axis": null,
-          },
-          "color": {
-            "value": MATRIX_HISTOGRAM_COLOR
-          },
-          "size": {
-            "value": MATRIX_HISTOGRAM_WIDTH
-          }
-        }
-      },
-      {
-        "hconcat": [
-          {
-            "layer": [
-              {
-                "mark": "square",
-                "width": MATRIX_WIDTH,
-                "height": MATRIX_HEIGHT,
-                "encoding": {
-                  "x": {
-                    "field": "omega",
-                    "type": "quantitative",
-                    scale: paaScale,
-                    "axis": null,
-                  },
-                  "y": {
-                    "field": "alpha",
-                    "type": "quantitative",
-                    scale: saxScale,
-                    "axis": null,
-                  },
-                  "size": {
-                    "value": 600
-                  },
-                  "color": {
-                    "field": "error",
-                    "type": "quantitative",
-                    "scale": {
-                      "scheme": "yellowgreenblue",
-                      domain: MATRIX_DOMAIN,
-                      "type": "log",
-                      "base": 10
-                    },
-                    "legend": null
-                  },
-                  "opacity": {
-                    "value": 1
-                  }
-                }
-              },
-              // {
-              //   "mark": "text",
-              //   "width": mainWidth,
-              //   "height": mainHeight,
-              //   "encoding": {
-              //     "x": {
-              //       "field": "omega",
-              //       "type": "quantitative",
-              //       scale: paaScale
-              //     },
-              //     "y": {
-              //       "field": "alpha",
-              //       "type": "quantitative",
-              //       scale: saxScale
-              //     },
-              //     "text": {
-              //       "field": "error",
-              //       "format": "2f"
-              //     }
-              //   }
-              // }
-            ]
-          },
-          {
-            "mark": "bar",
-            "height": MATRIX_HEIGHT,
-            "width": MATRIX_HISTOGRAM_HEIGHT,
-            "encoding": {
-              "y": {
-                "bin": {
-                  "maxbins": MAX_SAX
-                },
-                "field": "alpha",
-                "type": "quantitative",
-                "axis": null,
-                scale: saxScale
-              },
-              "x": {
-                "aggregate": "mean",
-                "field": "progress",
-                "type": "quantitative",
-                "scale": {
-                  "domain": [0, 100]
-                },
-                "axis": null,
-              },
-              "size": {
-                "value": MATRIX_HISTOGRAM_WIDTH
-              },
-              "color": {
-                "value": MATRIX_HISTOGRAM_COLOR
-              },
-            }
-          }
-        ]
-      }
-    ],
-    "config": {
-      "concat": {
-        "spacing": 0
-      },
-      "view": {
-        "stroke": "transparent"
-      },
-      "padding": 0
-    }
-  };
-
-  view = vegaEmbed("div#canvas", spec)
-    .then(res => res.view);
-}
 
 // data contains list of objects with 'cell' property for the parametrization and 'value' for the
 // new entry
@@ -227,4 +193,5 @@ function sendDataToFrontend(next_chunk) {
 
 window.eel.set_host("ws://localhost:8080");
 window.eel.register_client("hello there");
-window.eel.expose(sendRealData, "send_data");
+// window.eel.expose(sendRealData, "send_data");
+window.eel.expose(render, "send_data");
